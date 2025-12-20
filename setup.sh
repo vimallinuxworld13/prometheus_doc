4d7d6707a5a26517d040d901d1804dcef22b8bab
vimallinuxworld13 <vdaga@lwindia.com>
2025-12-20 16:38:54 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+for prom server:
+# wget https://github.com/prometheus/prometheus/releases/download/v3.5.0/prometheus-3.5.0.linux-amd64.tar.gz
+# tar -xvzf prometheus-3.5.0.linux-amd64.tar.gz
+# cd prometheus-3.5.0.linux-amd64/
+# ./prometheus
+
+for target linux node:
+# yum install wget -y
+# wget https://github.com/prometheus/node_exporter/releases/download/v1.10.2/node_exporter-1.10.2.linux-amd64.tar.gz
+# tar -xvzf node_exporter-1.10.2.linux-amd64.tar.gz
+# cd node_exporter-1.10.2.linux-amd64
+
+# ./node_exporter
+
+
